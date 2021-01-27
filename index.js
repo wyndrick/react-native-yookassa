61e32fef86bc336e79955545fac46284de29412c
wyndrick <wyndrick@gmail.com>
2021-01-27 20:51:07 +0200

    Работа над модулем оплат для веба

--- index.js
+++ index.js
@@ -1,6 +1,71 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
+let YookassaNative 
+if (Platform.OS !== 'web') {
+  console.log(widget)
+  YookassaNative = NativeModules.Yookassa
+  
+} else {
+  var shopId
+  YookassaNative = {
+    initialize:(_shopId, token, clientId)=> {
+      shopId = _shopId
+    },
+    tokenization:(title, desc, summ, paymentType, savePaymentMethod, testParameters) => {
+      const checkout = YooMoneyCheckoutUI(shopId, {
+        language: 'ru',
+        domSelector: 'body',
+        amount: summ
+      });
 
-const { Yookassa: YookassaNative } = NativeModules;
+      checkout.open()
+      try {
+        checkout.on('yc_error', response => {
+          console.log("yc_error", response)
+          /*
+            {
+                status: 'error',
+                error: {
+                    type: 'validation_error',
+                    message: undefined,
+                    status_code: 400,
+                    code: undefined,
+                    params: [
+                        {
+                            code: 'invalid_number',
+                            message: 'Неверный номер карты'
+                        },
+                        {
+                            code: 'invalid_expiry_month',
+                            message: 'Невалидное значение месяца'
+                        }
+                    ]
+                }
+            }
+          */
+        });
+        checkout.on('yc_success', response => {
+          console.log("yc_success", response)
+          checkout.chargeSuccessful();
+          /*
+          {
+              status: 'success',
+              data: {
+                  message: 'Токен для оплаты создан',
+                  status_code: 200,
+                  type: 'payment_token_created',
+                  response: {
+                      paymentToken: 'eyJlbmNyeXB0ZWRNZXNzYWdlIjoiWlc1amNubHdkR1ZrVFdWemMyRm5aUT09IiwiZXBoZW1lcmFsUHVibGljS2V5IjoiWlhCb1pXMWxjbUZzVUhWaWJHbGpTMlY1IiwidGFnIjoiYzJsbmJtRjBkWEpsIn0K'
+                  }
+              }
+          }
+          */
+        });
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
 
 /**
  * Enum for PaymentType.
@@ -25,10 +90,12 @@ const ResultStatus = Object.freeze({
   RESULT_CANCELED : "RESULT_CANCELED",
   RESULT_ERROR : "RESULT_ERROR",
 });
+
 class Yookassa {
   static paymentTypes = PaymentType
   static savePaymentMethods = SavePaymentMethod
   static initialize = (shopId, token, clientId = null) => {
+    console.log(YookassaNative)
     YookassaNative.initialize(shopId, token, clientId);
   }
   static tokenization = (title, desc, summ, paymentType, savePaymentMethod, testParameters) => {
